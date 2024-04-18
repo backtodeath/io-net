6298e23714ba31a4b82d876e2e320aab73145745
Luis Lirola <lirolaluis@gmail.com>
2024-04-18 23:40:43 +0200

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/expect -f
 
-# Set a reasonable timeout
+exp_internal 1  ;# Turn on expect internal debugging
 set timeout 300
 
 # Start the update process
 spawn sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confold" --force-confdef --allow-downgrades --allow-remove-essential --allow-change-held-packages
-
-# Handle potential prompt about sshd_config
 expect {
     "What do you want to do about modified configuration file sshd_config?" {
         send "2\r"
-        exp_continue  # Continue expecting other interactions if needed
+        exp_continue
+    }
+    timeout {
+        puts "Failed to handle sshd_config prompt or command timed out."
+        exit 1
     }
     eof {
-        # Catch end of file, which means the spawn command finished
+        # Installation of updates completed
     }
 }
 
-# Start the installation of necessary packages
+# Install necessary virtualization packages
 spawn sudo apt-get install -y -o Dpkg::Options::="--force-confold" --force-confdef --allow-downgrades --allow-remove-essential --allow-change-held-packages qemu-kvm libvirt-daemon-system virt-manager bridge-utils cloud-image-utils docker.io
-expect eof
+expect {
+    timeout {
+        puts "Installation of packages timed out or failed."
+        exit 1
+    }
+    eof {
+        puts "Installation of packages completed successfully."
+    }
+}
