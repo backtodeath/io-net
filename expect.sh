f6b30462d4baed88e66dbcf09bbb5b15217ae862
Luis Lirola <lirolaluis@gmail.com>
2024-04-18 23:43:01 +0200

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/expect -f
 
-exp_internal 1  ;# Turn on expect internal debugging
 set timeout 300
 
-# Start the update process
-spawn sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confold" --force-confdef --allow-downgrades --allow-remove-essential --allow-change-held-packages
+# Update packages
+spawn sudo apt-get update
+expect eof
+
+# Upgrade packages with appropriate options
+spawn sudo apt-get upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef"
 expect {
     "What do you want to do about modified configuration file sshd_config?" {
         send "2\r"
-        exp_continue
+        exp_continue  # Continue expecting other interactions if needed
     }
     timeout {
         puts "Failed to handle sshd_config prompt or command timed out."
@@ -20,13 +23,5 @@ expect {
 }
 
 # Install necessary virtualization packages
-spawn sudo apt-get install -y -o Dpkg::Options::="--force-confold" --force-confdef --allow-downgrades --allow-remove-essential --allow-change-held-packages qemu-kvm libvirt-daemon-system virt-manager bridge-utils cloud-image-utils docker.io
-expect {
-    timeout {
-        puts "Installation of packages timed out or failed."
-        exit 1
-    }
-    eof {
-        puts "Installation of packages completed successfully."
-    }
-}
+spawn sudo apt-get install -y qemu-kvm libvirt-daemon-system virt-manager bridge-utils cloud-image-utils docker.io
+expect eof
