aa32db1decb00a56ab8516f598fefc61676283fc
Luis Lirola <lirolaluis@gmail.com>
2024-04-18 23:51:09 +0200

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -6,17 +6,6 @@ set timeout 300
 spawn sudo apt-get update
 expect eof
 
-spawn sudo apt-get upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef"
-expect {
-    "What do you want to do about modified configuration file sshd_config?" {
-        send "2\r"
-        exp_continue
-    }
-    eof {
-        # Catch end of file, which means the command finished
-    }
-}
-
 # Install necessary virtualization packages
 spawn sudo apt-get install -y qemu-kvm libvirt-daemon-system virt-manager bridge-utils cloud-image-utils docker.io
 expect eof
